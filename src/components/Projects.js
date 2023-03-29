ac18a2d3e0394f8a3c4a47a18fe6d4d32dadc548
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-03-28 23:16:50 -0600

    fix extensions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,8 +12,8 @@ const ProjectData = [
     Summary: `Taste of Home is a casual puzzle game about cooking, memories, and family. Play as Kai as you attempt to recreate your Grandma's signature dish. Learn about your family's culture and origins as you embark on this cute little cooking adventure.`,
     Images: [
       {
-        original: '/UnityProjects/TasteOfHome/Gameplay/Intro.GIF',
-        thumbnail: '/UnityProjects/TasteOfHome/Gameplay/Intro.GIF',
+        original: '/UnityProjects/TasteOfHome/Gameplay/Intro.gif',
+        thumbnail: '/UnityProjects/TasteOfHome/Gameplay/Intro.gif',
       },
       {
         original: '/UnityProjects/TasteOfHome/Gameplay/1.PNG',
@@ -24,8 +24,8 @@ const ProjectData = [
         thumbnail: '/UnityProjects/TasteOfHome/Gameplay/2.PNG',
       },
       {
-        original: '/UnityProjects/TasteOfHome/Gameplay/Dialogue.GIF',
-        thumbnail: '/UnityProjects/TasteOfHome/Gameplay/Dialogue.GIF',
+        original: '/UnityProjects/TasteOfHome/Gameplay/Dialogue.gif',
+        thumbnail: '/UnityProjects/TasteOfHome/Gameplay/Dialogue.gif',
       },
       {
         original: '/UnityProjects/TasteOfHome/Gameplay/3.PNG',
@@ -36,8 +36,8 @@ const ProjectData = [
         thumbnail: '/UnityProjects/TasteOfHome/Gameplay/4.PNG',
       },
       {
-        original: '/UnityProjects/TasteOfHome/Gameplay/Cooking.GIF',
-        thumbnail: '/UnityProjects/TasteOfHome/Gameplay/Cooking.GIF',
+        original: '/UnityProjects/TasteOfHome/Gameplay/Cooking.gif',
+        thumbnail: '/UnityProjects/TasteOfHome/Gameplay/Cooking.gif',
       },
       {
         original: '/UnityProjects/TasteOfHome/Gameplay/5.PNG',
@@ -54,15 +54,15 @@ const ProjectData = [
     Title: "Soul's Path",
     URL: "SoulsPath",
     GamePath: "https://akchen.itch.io/souls-path",
-    Img: "UnityProjects/SoulsPath/thumbnail.PNG",
+    Img: "UnityProjects/SoulsPath/thumbnail.png",
     Skills: ["Unity"],
     Summary: `Soul’s path is a platformer game that follows Sol the shadow fox who was cursed and imprisoned in a cave eternally. To escape the cave, the shadow fox must harvest the souls of the spirit birds by killing them. In each level, Sol is challenged to jump from rock to rock to hunt for spirit fire. When enough spirit fire is harvested, the lantern will light up his way to the next level.
     \n
     But watch out, along the path there are dangerous vines and poisonous water. Failing to connect with a rock also kills Sol as he is a shadow that only survives in the dark.`,
     Images: [
       {
-        original: '/UnityProjects/SoulsPath/Gameplay/Intro.GIF',
-        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Intro.GIF',
+        original: '/UnityProjects/SoulsPath/Gameplay/Intro.gif',
+        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Intro.gif',
         description: '',
       },
       {
@@ -71,8 +71,8 @@ const ProjectData = [
         description: '',
       },
       {
-        original: '/UnityProjects/SoulsPath/Gameplay/Mechanic.GIF',
-        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Mechanic.GIF',
+        original: '/UnityProjects/SoulsPath/Gameplay/Mechanic.gif',
+        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Mechanic.gif',
         description: '',
       },
       {
@@ -80,13 +80,13 @@ const ProjectData = [
         thumbnail: '/UnityProjects/SoulsPath/Gameplay/2.PNG',
       },
       {
-        original: '/UnityProjects/SoulsPath/Gameplay/Platforms.GIF',
-        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Platforms.GIF',
+        original: '/UnityProjects/SoulsPath/Gameplay/Platforms.gif',
+        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Platforms.gif',
         description: '',
       },
       {
-        original: '/UnityProjects/SoulsPath/Gameplay/Transitions.GIF',
-        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Transitions.GIF',
+        original: '/UnityProjects/SoulsPath/Gameplay/Transitions.gif',
+        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Transitions.gif',
         description: '',
       },
       {
@@ -98,8 +98,8 @@ const ProjectData = [
         thumbnail: '/UnityProjects/SoulsPath/Gameplay/4.PNG',
       },
       {
-        original: '/UnityProjects/SoulsPath/Gameplay/Vines.GIF',
-        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Vines.GIF',
+        original: '/UnityProjects/SoulsPath/Gameplay/Vines.gif',
+        thumbnail: '/UnityProjects/SoulsPath/Gameplay/Vines.gif',
         description: '',
       },
       {
@@ -122,24 +122,24 @@ const ProjectData = [
     Summary: `Seishi has the power of controlling day and night. You must dodge the spirits' attacks and survive for as long as possible. Use WASD to move, space to switch between day and night. The two-faced spirits will only move and attack in their active time, otherwise they will freeze. You can use the frozen spirits as a shield against the active spirits and kill the frozen spirits. Surviving gives you point. Killing spirits gives you bonus points. Staying in bound and close to the tower will increase your points faster.`,
     Images: [
       {
-        original: '/UnityProjects/Mare/Gameplay/Light.GIF',
-        thumbnail: '/UnityProjects/Mare/Gameplay/Light.GIF',
+        original: '/UnityProjects/Mare/Gameplay/Light.gif',
+        thumbnail: '/UnityProjects/Mare/Gameplay/Light.gif',
       },
       {
         original: '/UnityProjects/Mare/Gameplay/1.PNG',
         thumbnail: '/UnityProjects/Mare/Gameplay/1.PNG',
       },
       {
-        original: '/UnityProjects/Mare/Gameplay/Switch.GIF',
-        thumbnail: '/UnityProjects/Mare/Gameplay/Switch.GIF',
+        original: '/UnityProjects/Mare/Gameplay/Switch.gif',
+        thumbnail: '/UnityProjects/Mare/Gameplay/Switch.gif',
       },
       {
         original: '/UnityProjects/Mare/Gameplay/2.PNG',
         thumbnail: '/UnityProjects/Mare/Gameplay/2.PNG',
       },
       {
-        original: '/UnityProjects/Mare/Gameplay/Death.GIF',
-        thumbnail: '/UnityProjects/Mare/Gameplay/Death.GIF',
+        original: '/UnityProjects/Mare/Gameplay/Death.gif',
+        thumbnail: '/UnityProjects/Mare/Gameplay/Death.gif',
       },
       {
         original: '/UnityProjects/Mare/Gameplay/3.PNG',
@@ -157,16 +157,16 @@ const ProjectData = [
     Summary: `Rearrange the furniture around you to protect yourself from the earthquake!`,
     Images: [
       {
-        original: '/UnityProjects/Ouch/Gameplay/Win.GIF',
-        thumbnail: '/UnityProjects/Ouch/Gameplay/Win.GIF',
+        original: '/UnityProjects/Ouch/Gameplay/Win.gif',
+        thumbnail: '/UnityProjects/Ouch/Gameplay/Win.gif',
       },
       {
         original: '/UnityProjects/Ouch/Gameplay/1.PNG',
         thumbnail: '/UnityProjects/Ouch/Gameplay/1.PNG',
       },
       {
-        original: '/UnityProjects/Ouch/Gameplay/Moving.GIF',
-        thumbnail: '/UnityProjects/Ouch/Gameplay/Moving.GIF',
+        original: '/UnityProjects/Ouch/Gameplay/Moving.gif',
+        thumbnail: '/UnityProjects/Ouch/Gameplay/Moving.gif',
       },
       {
         original: '/UnityProjects/Ouch/Gameplay/2.PNG',
